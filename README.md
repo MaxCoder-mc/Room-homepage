d107a5685181ea339deeb41726a076e30644c078
MaxCoder-mc <69950951+MaxCoder-mc@users.noreply.github.com>
2025-06-20 17:28:42 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+# Frontend Mentor - Room homepage solution
+
+This is a solution to the [Room homepage challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/room-homepage-BtdBY_ENq).
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Navigate the slider using either their mouse/trackpad or keyboard
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
---
+++ script.js
@@ -0,0 +1,88 @@
+const slides = document.querySelectorAll(".slide");
+const menuToggle = document.getElementById("menuToggle");
+const nav = document.querySelector(".nav");
+const menuImg = menuToggle.querySelector("img");
+const overlay = document.querySelector(".overlay");
+
+function openMenu() {
+  nav.classList.add("active");
+  overlay.classList.remove("hidden");
+  menuImg.src = "./images/icon-close.svg";
+  menuToggle.ariaLabel = "Close Menu";
+}
+function closeMenu() {
+  nav.classList.remove("active");
+  overlay.classList.add("hidden");
+  menuImg.src = "./images/icon-hamburger.svg";
+  menuToggle.ariaLabel = "Open Menu";
+}
+// Add event listeners for the menu toggle button
+menuToggle.addEventListener("click", () => {
+  if (nav.classList.contains("active")) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+});
+
+// Close the menu when clicking a link
+const navLinks = document.querySelectorAll(".nav__list a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    closeMenu();
+  });
+});
+
+//Close the menu when clicking elsewhere
+overlay.addEventListener("click", () => {
+  closeMenu();
+});
+
+// Remove the active class when resizing the window
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 1200) {
+    closeMenu();
+  }
+});
+
+//Create nav-arrows to be attached to the active class
+const navArrows = document.createElement("div");
+navArrows.className = "nav-arrows";
+navArrows.innerHTML = `
+          <button id="prev" class="nav-arrows_btn">
+            <img src="./images/icon-angle-left.svg" alt="" />
+          </button>
+          <button id="next" class="nav-arrows_btn">
+            <img src="./images/icon-angle-right.svg" alt="" />
+          </button>
+      `;
+
+// Function to append arrows to current slide
+function appendArrowsToSlide(index) {
+  slides[index].querySelector(".slide-picture").appendChild(navArrows);
+}
+appendArrowsToSlide(0);
+
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+let index = 0;
+// Function to show the current slide with animation
+// and remove the previous animation class
+function showSlide(i, animationClass) {
+  slides.forEach((slide, idx) => {
+    slide.classList.remove("slide-in-left", "slide-in-right");
+    slide.classList.toggle("active-slide", idx === i);
+    appendArrowsToSlide(i);
+    slide.classList.add(animationClass);
+  });
+}
+
+next.addEventListener("click", () => {
+  index = (index + 1) % slides.length;
+  showSlide(index, "slide-in-right");
+});
+
+prev.addEventListener("click", () => {
+  index = (index - 1 + slides.length) % slides.length;
+  showSlide(index, "slide-in-left");
+});
